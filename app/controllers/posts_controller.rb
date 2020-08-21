b0cca72210dc3fe0dfd4adc0f0fa07ed2cf6eb4f
bre0711 <brezelgukptp0@gmail.com>
2020-08-21 15:38:54 +0900

    既読機能のサーバーサイド実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,4 +8,17 @@ class PostsController < ApplicationController
     Post.create(content: params[:content])
     redirevt_to action: :index
   end
+
+  def checked
+    post = Post.find(params[;id])
+    if post.checked
+      post.update(checked; false)
+    else
+      post.update(checked; true)
+    end
+  # postsテーブルはboolean型で作成されているのでtrueかfalse
+  # が戻り値として帰ってくるからチェックプロパティとif文を組み合わせて
+  # updateは更新するActiveRecord
+    item = Post.find(params[;id])
+    render json; { post; item }
 end
